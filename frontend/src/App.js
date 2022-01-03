f11ab08e5a7d78d5fe2d5de61ec293659b8e0ddc
Amanuel <anegussie9077@conestogac.on.ca>
2022-01-03 01:20:46 -0500

    Render function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {Link,Route,Routes} from "react-router-dom"
+import {Link,NavLink,Route,Routes} from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import AddReview from "./components/add-review";
@@ -8,76 +8,32 @@ import RestaurantsList from "./components/restaurants-list";
 import Login from "./components/login";
 
 
-class App extends Component () {
-  constructor(props){
-    super(props);
-    this.state = {
-      user:null
-    };
-  }
-  async login(user = null) {
-    this.setUser(user);
+function App() {
+  const [user, setUser] = React.useState(null);
+
+  async function login(user = null) {
+    setUser(user);
   }
 
-  async  logout() {
-    this.setUser(null)
+  async function logout() {
+    setUser(null)
   }
 
-  render () {
-    return (
-      
-      <div>
-         <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <a href="/restaurants" className="navbar-brand">
-          Restaurant Reviews
+  return (
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+      <a href="/restaurants" className="navbar-brand">
+        Restaurant Reviews 
         </a>
         <div className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <Link to={"/restaurants"} className="nav-link">
-              Restaurants
-            </Link>
-          </li>
-          <li className="nav-item" >
-            { this.user ? (
-              <a onClick={()=> this.setUser(null)} className="nav-link" style={{cursor:'pointer'}}>
-                Logout {this.user}
-              </a>
-            ) : (            
-            <Link to={"/login"} className="nav-link">
-              Login
-            </Link>
-            )}
+        <li className="nav-item">
+         
           </li>
         </div>
+
       </nav>
-
-      <div className="container mt-3">
-        <Routes>
-          <Route path={["/", "/restaurants"]} element={<RestaurantsList/>} />
-          <Route 
-            path="/restaurants/:id/review"
-            render={(props) => (
-              <AddReview {...props} user={this.user} />
-            )}
-          />
-          <Route 
-            path="/restaurants/:id"
-            render={(props) => (
-              <Restaurant {...props} user={this.user} />
-            )}
-          />
-          <Route 
-            path="/login"
-            render={(props) => (
-              <Login {...props} login={this} />
-            )}
-          />
-        </Routes>
-      </div>
-      </div>
-
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
